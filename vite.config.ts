aa7d5bb85495bf4a921338d2055e2b924f850fde
sarafashka <sarafanova.nv@gmail.com>
2024-05-13 23:54:24 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  };
-
   // const config = {
   //   plugins: [react()],
-  //   base: '/backtesting-app',
+  //   base: '/',
   // };
+
+  const config = {
+    plugins: [react()],
+    base: '/backtesting-app',
+  };
 
   return config;
 });
